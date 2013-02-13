c13aa22d3064bc56b8b5b883099542a6feb6c358
高田 千裕 <chihiro@takada-youhei-no-macbook.local>
2013-02-13 21:34:57 +0900

    copy script that count textline.

---
+++ python/textlinecounter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+
+import string
+
+def count_lines_in_text(text):
+	'''
+	text の行数を数えて返す
+	'''
+	lines = text.split("\n")
+	result = len(lines)
+	
+	return result
+	
+	
+def count_lines_in_file(filepath):
+	'''
+	テキストファイルを読み込み、行数を数えて返す
+	'''
+	result = 0
+	
+	with open(filepath, 'r', encoding='utf-8') as fp:
+		text = fp.read()
+		result = count_lines_in_text(text)
+	
+	return result
---
+++ python/textlinecounter_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+
+import textlinecounter
+import unittest
+
+class TestGlobalFunctions(unittest.TestCase):
+	def setUp(self):
+		pass
+		
+	def tearDown(self):
+		pass
+		
+	def test_count_lines_in_text(self):
+		self.assertEqual(1, textlinecounter.count_lines_in_text('abc'))
+		self.assertEqual(2, textlinecounter.count_lines_in_text("abc\n"))
+		self.assertEqual(1, textlinecounter.count_lines_in_text(''))
+		self.assertEqual(1, textlinecounter.count_lines_in_text('あいうえお'))
+		self.assertEqual(3, textlinecounter.count_lines_in_text("あ\nい\nう"))
+		pass
+	
+	
+	def test_count_lines_in_file(self):
+		self.assertEqual(10, textlinecounter.count_lines_in_file('data_test/utf8_01.txt'))
+		pass
+	
+		
+if __name__ == '__main__':
+	unittest.main()
+	
