7b79470f9d22e94491e3deba9978bc73a19f97b6
Takada Yohei <takadayouhei@gmail.com>
2013-06-22 22:38:54 +0900

    change table struct

--- php/deliverables/insert_record.php
+++ php/deliverables/insert_record.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>レコード追加</title>
+</head>
+<body>
 <?php
 // insert_record.php
 // 概要
@@ -6,21 +13,68 @@
 //   パラメータ
 //     encode …... エンコード
 //     id_parent ... 親id
-//     種類
-//     グループ
-//     概要
-//     詳細
+//     type ... 種類
+//     group ... グループ
+//     brief ... 概要
+//     detail ... 詳細
 // 共通処理を別ファイルに分離
 require_once(dirname(__FILE__) . '/common.php');
+
 
 /// テーブル作成
 function insert_record($argv)
 {
+	
+	//-----
 	if (!array_key_exists('encode', $argv))
 	{
+		print ('ERROR:no encode.');
 		return;
 	}
 	$encode = $argv['encode'];
+	
+	//-----
+	if (!array_key_exists('id_parent', $argv))
+	{
+		print ('ERROR:no id_parent.');
+		return;
+	}
+	$id_parent = $argv['id_parent'];
+	
+	//-----
+	if (!array_key_exists('type', $argv))
+	{
+		print ('ERROR:no type.');
+		return;
+	}
+	$type = $argv['type'];
+	
+	//-----
+	if (!array_key_exists('group', $argv))
+	{
+		print ('ERROR:no group.');
+		return;
+	}
+	$group = $argv['group'];
+	
+	//-----
+	if (!array_key_exists('brief', $argv))
+	{
+		print ('ERROR:no brief.');
+		return;
+	}
+	$brief = $argv['brief'];
+	
+	//-----
+	if (!array_key_exists('detail', $argv))
+	{
+		print ('ERROR:no detail.');
+		return;
+	}
+	$detail = $argv['detail'];
+	
+	
+	
 	
 	// 以下無視
 	return;
@@ -91,24 +145,20 @@ EOD;
 
 insert_record($_POST);
 
-?><!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>レコード追加</title>
-</head>
-<body>
+$def_encode = 'aiueo';
+$def_id_parent = 'ia_parent';
+$def_type = 'def type';
+?>
 <h1>レコード追加</h1>
 
 <form action="./insert_record.php" method="POST">
-	/* 親のID */
-	/* このレコードの更新日時 */
-	/* 種類。成果物'data' プロセス'process' */
-	/* グループ化とかしたくなる ? */
-	/* 概要 */
-	/* 詳細 */
   <ul>
-    <li>エンコード　<input type="text" name="encode" /></li>
+    <li>エンコード　<input type="text" name="encode" value="<?php echo $def_encode; ?>"/></li>
+    <li>親id <input type="text" name="id_parent" value="<?php echo $def_id_parent; ?>"/></li>
+    <li>種類 <input type="text" name="type" value="<?php echo $def_type; ?>"/></li>
+    <li>グループ <input type="text" name="group" value=""/></li>
+    <li>概要 <input type="text" name="brief" value=""/></li>
+    <li>詳細 <input type="text" name="detail" value=""/></li>
   </ul>
   <input type="submit" value="送信" />
 </form>
--- php/textfiledb/create_database.php
+++ php/textfiledb/create_database.php
@@ -7,47 +7,77 @@
 // 共通処理を別ファイルに分離
 require_once(dirname(__FILE__) . '/common.php');
 
-$sql_create = <<<EOD
+function create_database()
+{
+
+	// filename ... ファイル名 (含む拡張子) ex: 'sample.txt'
+	// fileext ... ファイルの拡張子。 '.' は含まない。 ex: 'txt', 'csv'
+	// folderpath ... ファイルが置かれているパス
+	// detail ... ファイルの内容
+	// lastupdate .... 最終更新日
+	// revision .... 更新に絡む情報
+	$sql_create = <<<EOD
 CREATE TABLE IF NOT EXISTS master_data (
-	filename    CHAR(20)  DEFAULT 'no name',
+	filename    CHAR(255) DEFAULT 'no name',
+	fileext     CHAR(255) DEFAULT '',
 	folderpath  CHAR(255) DEFAULT '.',
-	detail      TEXT      DEFAULT '',
+	detail      LONGTEXT  DEFAULT '',
+	lastupdate  DATETIME  DEFAULT '2000-01-01 00:00:00',
+	revision    TEXT      DEFAULT '',
 	
 	PRIMARY KEY(filename)
 );
 EOD;
-
-$sql_drop = <<<EOD
+	
+	$sql_drop = <<<EOD
 DROP TABLE IF EXISTS master_data;
 EOD;
-
-try {
-	// MySQLサーバへ接続
-	$pdo = new_PDO();
 	
-	// すでにあったら削除
-	$stmt = $pdo->prepare($sql_drop);
-	$ret = $stmt->execute();
-	if (!$ret)
-	{
-		show_pdo_error($stmt);
-	}
-
-	// 改めて作りなおす
-	$stmt = $pdo->prepare($sql_create);
-	$ret = $stmt->execute();
-	if (!$ret)
-	{
-		show_pdo_error($stmt);
+	try {
+		// MySQLサーバへ接続
+		$pdo = new_PDO();
+		
+		// すでにあったら削除
+		$stmt = $pdo->prepare($sql_drop);
+		$ret = $stmt->execute();
+		if (!$ret)
+		{
+			show_pdo_error($stmt);
+			return FALSE;
+		}
+	
+		// 改めて作りなおす
+		$stmt = $pdo->prepare($sql_create);
+		$ret = $stmt->execute();
+		if (!$ret)
+		{
+			show_pdo_error($stmt);
+			return FALSE;
+		}
+		
+	} catch(PDOException $e){
+		echo 'exception!!';
+		echo $e->getMessage();
+		var_dump($e->getMessage());
+		
+		return FALSE;
 	}
 	
-} catch(PDOException $e){
-	echo 'exception!!';
-	echo $e->getMessage();
-	var_dump($e->getMessage());
+	// 切断
+	$pdo = null;
+	
+	return TRUE;
 }
 
-// 切断
-$pdo = null;
+// 関数呼び出し
+$ret = create_database();
+if ($ret)
+{
+	echo 'success!!';
+}
+else
+{
+	echo 'failed!!';
+}
 
 ?>
