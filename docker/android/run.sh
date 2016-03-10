91637093629cba26c9f5bb99a8496b297921b853
takada youhei <TakadaYouhei@users.noreply.github.com>
2016-03-10 23:22:47 +0000

    ボリュームの設定追加
    コンテナ作り直しても残るように

--- docker/android/run.sh
+++ docker/android/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-docker run --rm -it -v /home/dockervolume:/home/dockervolume -w /home/dockervolume yoheitakada/android /bin/bash
+PATH_VOL_SRCHOST=/home/dockervolume
+PATH_VOL_CONTAINER=/home/dockervolume
+
+docker run --rm -it -v $PATH_VOL_SRCHOST:$PATH_VOL_CONTAINER -w $PATH_VOL_CONTAINER yoheitakada/android /bin/bash
 
