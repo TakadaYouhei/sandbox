019dd4edc8c42f5f6f52d4adb8fee810b39c7d04
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-02-02 22:09:22 +0900

    connect 後の処理を別スレッドにまかせて接続待ちを継続するように

--- python/listen_tcp_port.py
+++ python/listen_tcp_port.py
@@ -13,6 +13,17 @@ def usage():
     print("Usage: python3 listen_tcp_port.py <port>")
     exit(1)
 
+# 別スレッドで accept で受け取ったソケットを処理する
+def handle_client(conn, addr):
+    with conn:
+        print(f'Connected by {addr}')
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f'Received: {data.decode()}')
+
+
 # メイン処理
 def main():
     if len(sys.argv) != 2:
@@ -26,14 +37,13 @@ def main():
         s.bind((host, port))
         s.listen(1)
         print(f'Listening on {host}:{port}...')
-        conn, addr = s.accept()
-        with conn:
-            print(f'Connected by {addr}')
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f'Received: {data.decode()}')
+        # 繰り返し接続を待つ
+        while True:
+            conn, addr = s.accept()
+
+            # 接続を確認したらスレッドを作成して socket を処理する
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
 
 def loop(thread):
     try:
